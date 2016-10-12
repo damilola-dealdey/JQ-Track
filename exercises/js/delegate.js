8300e79ca96764929e199c189869ef8f35f7c44c
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-10-12 11:16:45 +0100

    Exercise 7.2 done

---
+++ exercises/js/delegate.js
@@ -0,0 +1,42 @@
+function Delegate() {
+  this.counter = 0;
+  this.init();
+}
+
+Delegate.prototype.init = function () {
+  var _this = this;
+  this.mainContainer = $('#main-container');
+  this.addButton = $('#add-button');
+  this.addButton.click(function () {
+    _this.add();
+  });
+
+  $("body").on("click", "div.attatched", function () {
+    _this.hit(this);
+  });
+};
+
+Delegate.prototype.add = function () {
+  var newDiv = $('<div class="attatched"></div>');
+  newDiv.data('index', ++this.counter);
+  newDiv.append($('<h1>My Number is ' + this.counter + '</h1>'));
+
+  this.mainContainer.append(newDiv);
+};
+
+Delegate.prototype.hit = function(elem){
+  var index = $(elem).data('index');
+  if(index == this.counter){
+    $(elem).remove();
+    this.counter--;
+    return;
+  }
+
+  $("div.attatched").filter('.active').each(function(){
+    $(this).removeClass('active');
+  });
+
+  $(elem).addClass('active');
+};
+
+var delgado = new Delegate();
